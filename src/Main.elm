e90af2a61720bc110c0a4d4c6006373fdb45f2f4
Ryan Rosenfeld <rrosenfeld@atlassian.com>
2020-11-06 09:38:47 -0800

    First tutorial

--- src/Main.elm
+++ src/Main.elm
@@ -12,6 +12,7 @@ module Main exposing (..)
 --     https://elm-lang.org/docs/syntax
 --
 
+import Array
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
@@ -30,12 +31,16 @@ main =
 
 
 type alias Model =
-    Int
+    { value : Int
+    , previousValues : List Int
+    }
 
 
 init : Model
 init =
-    0
+    { value = 0
+    , previousValues = []
+    }
 
 
 
@@ -45,16 +50,37 @@ init =
 type Msg
     = Increment
     | Decrement
+    | Square
+    | Undo
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            { previousValues = [ model.value ] ++ model.previousValues
+            , value = model.value + 3
+            }
 
         Decrement ->
-            model - 1
+            { previousValues = [ model.value ] ++ model.previousValues
+            , value = model.value - 1
+            }
+
+        Square ->
+            { previousValues = [ model.value ] ++ model.previousValues
+            , value = model.value * model.value
+            }
+
+        Undo ->
+            case model.previousValues of
+                [] ->
+                    model
+
+                _ ->
+                    { value = Maybe.withDefault 0 (List.head model.previousValues)
+                    , previousValues = Maybe.withDefault [] (List.tail model.previousValues)
+                    }
 
 
 
@@ -65,6 +91,8 @@ view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
+        , div [] [ text (String.fromInt model.value) ]
         , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Square ] [ text "square" ]
+        , button [ onClick Undo ] [ text "undo" ]
         ]
